a2e1f5c6b2c4882e70e29a27b3aeb6292db520f5
hyayi <98936899+hyayi@users.noreply.github.com>
2022-07-05 11:14:05 +0900

    Update model.py

--- model.py
+++ model.py
@@ -5,6 +5,24 @@ import monai
 import torch
 from torchmetrics import AUROC
 from Resnet50 import resnet50, resnet50_m
+import numpy as np
+import random
+pl.seed_everything(42)
+
+def torch_seed(random_seed=42):
+
+    torch.manual_seed(random_seed)
+
+    torch.cuda.manual_seed(random_seed)
+    torch.cuda.manual_seed_all(random_seed) # if use multi-GPU
+
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    np.random.seed(random_seed)
+    random.seed(random_seed)
+    
+torch_seed(42)
 
 
 class MRSClassfication(pl.LightningModule):
